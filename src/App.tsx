d111b0cee801c78193cd64b45c1733721065ea17
jaycsee <36982207+jaycsee@users.noreply.github.com>
2022-09-17 16:41:37 -0400

    Start work

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box, TextField } from "@mui/material";
+import React, { useState } from "react";
 
 function App() {
+  const [story, setStory] = useState("");
+
+  console.log(
+    fetch("https://api.cohere.ai/generate", {
+      method: "POST",
+      headers: {
+        Authorization: "BEARER ",
+        "Content-Type": "application/json",
+        "Cohere-Version": "2021-11-08",
+      },
+      body: JSON.stringify({
+        prompt: "Once upon a time in a magical land called",
+      }),
+    })
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box
+      display="flex"
+      flexDirection="column"
+      sx={{ m: 0, p: 0, width: "100vw", height: "100vh" }}
+    >
+      <Box>one word stories</Box>
+      <Box sx={{ width: "40rem", pt: 1, px: 3 }}>
+        <TextField fullWidth placeholder="API Key" size="small" />
+      </Box>
+      <Box flexGrow={1} sx={{ pt: 2, px: 3 }}>
+        <TextField
+          fullWidth
+          multiline
+          placeholder="Start your story here"
+          value={story}
+          onChange={(event) => setStory(event.target.value)}
+          sx={{ maxHeight: "100%" }}
+          rows={30}
+        />
+      </Box>
+    </Box>
   );
 }
 
